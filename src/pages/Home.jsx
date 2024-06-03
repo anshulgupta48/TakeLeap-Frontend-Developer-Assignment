5a3f460c8b1d1eb9c67dca990647a5c8420d5172
anshulgupta48 <anshulgupta4905@gmail.com>
2024-06-03 16:05:35 +0530

    Adding all Files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ const Home = (props) => {
     const [collegeDetails, setCollegeDetails] = useState([]);
     const getOptionLabel = (option) => option.name;
     const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -14,10 +15,15 @@ const Home = (props) => {
 
     const fetchCollegeDetails = async () => {
         setLoading(true);
-        const response = await fetch('http://universities.hipolabs.com/search');
-        const data = await response.json();
-        const uniqueCollegeDetails = removeDuplicateCollegeDetails(data);
-        setCollegeDetails(uniqueCollegeDetails);
+        setError(null);
+        try {
+            const response = await fetch('http://universities.hipolabs.com/search', { referrerPolicy: "unsafe-url" });
+            const data = await response.json();
+            const uniqueCollegeDetails = removeDuplicateCollegeDetails(data);
+            setCollegeDetails(uniqueCollegeDetails);
+        } catch (error) {
+            setError("Something Went Wrong");
+        }
         setLoading(false);
     };
 
@@ -35,7 +41,7 @@ const Home = (props) => {
     return (
         <div className='container'>
             <h2 className='title'>Welcome to College-Search Website</h2>
-            {loading ? <p className='loader'>Loading....</p> : <Autocomplete
+            {loading ? <p className='loader'>Loading....</p> : error ? <p className='loader'>{error}</p> : <Autocomplete
                 value={props.selectedOption}
                 onChange={(event, newValue) => { props.setSelectedOption(newValue) }}
                 onSelect={handleCollegeSelect}
